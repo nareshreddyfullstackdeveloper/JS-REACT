36f740e775d33617789beb74d5adce5e407252c7
nareshreddyfullstackdeveloper <98952876+nareshreddyfullstackdeveloper@users.noreply.github.com>
2022-04-16 18:00:03 +0530

    my first commit

---
+++ back up/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bbg-dark">
+                <a href= "#" className="navbar-brand">Reate State Example</a>
+
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
